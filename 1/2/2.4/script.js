eabdd6d0939f5f0761501a4476ea476337075f8d
Anna <novgorodova.anya@gmail.com>
2022-06-23 15:24:04 +0300

    Create script.js

---
+++ 1/2/2.4/script.js
@@ -0,0 +1,10 @@
+let admin;
+let name = "Джон";
+admin = name;
+alert(admin);
+
+let ourPlanet;
+let currentVisitor;
+
+const BIRTHDAY = '18.04.1982'; // здесь используем заглавные буквы
+const age = someCode(BIRTHDAY); // десь не используем,т.к. возраст меняется
